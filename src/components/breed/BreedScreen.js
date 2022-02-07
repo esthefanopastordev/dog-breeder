add2001a3975ae0f4cb3d5db71171589208e817e
Esthefano Pastor <esthefanopastordev@gmail.com>
2022-02-06 22:17:37 -0500

    Filtrar breeds por first letter

--- src/components/breed/BreedScreen.js
+++ src/components/breed/BreedScreen.js
@@ -1,17 +1,50 @@
+import { useEffect, useState } from 'react';
+
 import { useParams } from 'react-router-dom';
+
+import './BreedScreen.css';
 
 import { capitalizeText } from '../../util/capitalizeText';
 
 export const BreedScreen = () => {
+	const [urlImage, setUrlImage] = useState('');
 	const { breedName } = useParams();
 
-	console.log(breedName);
+	useEffect(() => {
+		const getRandomImageFromBreed = async () => {
+			let url = '';
+
+			if (breedName.includes('-')) {
+				url = `https://dog.ceo/api/breed/${breedName.replace(
+					'-',
+					'/'
+				)}/images/random`;
+			} else {
+				url = `https://dog.ceo/api/breed/${breedName}/images/random`;
+			}
+
+			const resp = await fetch(url);
+			const { message } = await resp.json();
+			setUrlImage(message);
+		};
+
+		getRandomImageFromBreed();
+	}, []);
 
 	return (
-		<main className="container breed-screen__container">
-			<h2>Look at this cute {capitalizeText(breedName.replace('-', ' '))}</h2>
+		<main className="breed-screen__container container text-center">
+			<h2 className="mb-4">
+				Look at this cute {capitalizeText(breedName.replace('-', ' '))}
+			</h2>
 
-			<img src="" alt="" />
+			<div
+				className="breed-screen__img mb-4"
+				style={{ backgroundImage: `url(${urlImage})` }}
+			></div>
+
+			<button type="button" className="btn btn-dark">
+				get another image
+			</button>
 		</main>
 	);
 };
--- src/components/breeds/BreedGrid.js
+++ src/components/breeds/BreedGrid.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -7,32 +7,62 @@ import { getAlphabet } from '../../util/getAlphabet';
 import './BreedGrid.css';
 
 import { BreedGridItem } from './BreedGridItem';
+import { useSearchParams } from 'react-router-dom';
 
 export const BreedGrid = ({ breeds = [] }) => {
-	const [currentLetter, setCurrentLetter] = useState('');
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [filteredBreeds, setFilteredBreeds] = useState([]);
+
+	const currentLetter = searchParams.get('letter');
+
+	useEffect(() => {
+		const letter = searchParams.get('letter');
+
+		if (letter) {
+			const filtered = breeds.filter(
+				breed => breed.name.charAt(0) === letter.toLowerCase()
+			);
+
+			setFilteredBreeds(filtered);
+		} else {
+			setFilteredBreeds(breeds);
+		}
+	}, [breeds, searchParams]);
+
+	const handleFilterByLetter = (letter = '') => setSearchParams({ letter });
 
 	return (
 		<>
 			<h1 className="mb-4">Breeds</h1>
 
 			<ol className="alphabet-filter mb-4">
-				<li className={!currentLetter ? 'active' : ''}>All</li>
+				<li
+					className={!currentLetter ? 'active' : ''}
+					onClick={() => handleFilterByLetter()}
+				>
+					All
+				</li>
 
 				{getAlphabet().map(letter => (
 					<li
 						key={letter}
 						className={currentLetter === letter.toLowerCase() ? 'active' : ''}
+						onClick={() => handleFilterByLetter(letter.toLowerCase())}
 					>
 						{letter}
 					</li>
 				))}
 			</ol>
 
-			<div className="cards-container">
-				{breeds.map(breed => (
-					<BreedGridItem key={breed.name} breed={breed} />
-				))}
-			</div>
+			{filteredBreeds.length > 0 ? (
+				<div className="cards-container">
+					{filteredBreeds.map(breed => (
+						<BreedGridItem key={breed.name} breed={breed} />
+					))}
+				</div>
+			) : (
+				<h4 className="text-center">Nothing to show 😢</h4>
+			)}
 		</>
 	);
 };
